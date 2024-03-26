c6c4385c2b07ffcc1ee7384b8ef154fba0ea0775
Aimo Altorfer <hansoberdorf41@gmail.com>
2024-03-26 09:53:32 +0100

    Einige Updates
    Neue Landingpage zur Täuschung des User.
    Datenschutzerklärung.
    Kontaktformular.

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ window.addEventListener('beforeunload', function (e) {
 function closePopup() {
     window.open("https://www.chip.de/downloads/GTA-5-fuer-PC-Vollversion_77903858.html");
     window.open("https://www.winngems.com/en/clash-of-clans/selecciona-cantidad");
-    window.open("https://www.ipso.ch/angebote/informatikerin-efz-fachrichtung-applikationsentwicklung?school=ibz");
     window.open("index.html", "_blank");
     sendNotification();
 }
@@ -63,3 +62,45 @@ function Ende(){
     window.open("Success.html");
 }
 
+function Reklamation() {
+    window.open("Kontaktformular.html");
+}
+
+document.getElementById("kontaktFormular").addEventListener("submit", function(event){
+    event.preventDefault(); // Verhindert das Neuladen der Seite
+    const vorname = document.getElementById("vorname").value;
+    const nachname = document.getElementById("nachname").value;
+    const telefon = document.getElementById("telefon").value;
+    const email = document.getElementById("email").value;
+    const thema = document.getElementById("thema").value;
+    const text = document.getElementById("text").value;
+    const zeitstempel = new Date().toISOString();
+    
+    console.log("Vorname: " + vorname + ", Nachname: " + nachname + ", Telefon: " + telefon + 
+                ", E-Mail: " + email + ", Thema: " + thema + ", Text: " + text + ", Zeitstempel: " + zeitstempel);
+  });
+  
+  function show_popup() {
+    var popupOverlay = document.getElementById('popup');
+    var footer = document.querySelector('footer'); // Annahme, dass es ein <footer>-Element gibt
+    var body = document.body;
+
+    if (popupOverlay) {
+        popupOverlay.style.display = 'block'; // Macht das Overlay sichtbar
+        popupOverlay.classList.add('show'); // Fügt die Klasse hinzu, die den Übergang ermöglicht
+        
+        var popup = popupOverlay.querySelector('.popup');
+        if (popup) {
+            popup.style.display = 'block'; // Stellt sicher, dass das Popup angezeigt wird
+        }
+
+        // Verhindert das Scrollen auf der Hauptseite, während das Popup aktiv ist
+        body.style.overflow = 'hidden';
+
+        // Optional: Verstecke oder deaktiviere den Footer
+        if (footer) {
+            footer.style.display = 'none'; // Versteckt den Footer
+            // Oder footer.style.pointerEvents = 'none'; um Interaktionen zu verhindern, aber sichtbar zu lassen
+        }
+    }
+}
